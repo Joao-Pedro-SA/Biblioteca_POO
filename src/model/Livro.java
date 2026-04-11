2ea5e5d92e7477c632e4e284a1e5d3879c1dd6e8
Gsidoka <guisting1@gmail.com>
2026-04-11 15:20:25 -0300

    feat: implementação da classe Livro

--- src/model/Livro.java
+++ src/model/Livro.java
@@ -1,4 +1,33 @@
 package model;
 
 public class Livro {
+    private int id;
+    private String titulo;
+    private String autor;
+    private boolean disponivel;
+    
+    public Livro(String titulo, String autor) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponivel = true;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public String toString() {
+        return "Livro{id=" + id + ", titulo='" + titulo + "', autor='" + autor + "', disponivel=" + disponivel + "}";
+    }
 }
